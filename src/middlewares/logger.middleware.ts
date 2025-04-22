035cee8c72cc0fa2fb091b4454794f8eb07e23ef
Alireza Valizadeh <alirezavalizadeh1999@gmail.com>
2025-04-22 21:40:07 +0330

    chore: add a test enter

--- src/middlewares/logger.middleware.ts
+++ src/middlewares/logger.middleware.ts
@@ -7,12 +7,14 @@ export class LoggerMiddleware implements NestMiddleware {
   constructor(private readonly logger: MyLoggerService) {}
   use(req: Request, res: Response, next: NextFunction) {
     const { method, originalUrl } = req;
+    
     this.logger.http(`---------------------------------------`);
     this.logger.http(`${method} ${originalUrl}`);
     res.on('finish', () => {
       const { statusCode } = res;
       const contentLength = res.get('content-length') || '0';
 
+      
       this.logger.http(
         `Execution took ${contentLength}ms - Finished with Status: ${statusCode}`,
       );
