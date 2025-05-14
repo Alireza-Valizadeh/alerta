502f37063e07ba5db1d6507cfd20a970bbcc91e8
Alireza Valizadeh <alirezavalizadeh1999@gmail.com>
2025-05-14 18:24:53 +0330

    fix: add cors rules to main app

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3001',
+  });
   app.enableVersioning({
     type: VersioningType.URI,
     defaultVersion: '1',
