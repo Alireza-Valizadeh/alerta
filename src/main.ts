d138dcd250b70b6780e27f04b821bcfe6db2ea1e
Alireza Valizadeh <alirezavalizadeh1999@gmail.com>
2025-05-15 11:52:39 +0330

    feat: update cors rules and add global api prefix

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,9 @@ import { VersioningType } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3001', 'http://192.168.1.79:3001'],
+    origin: '*',
   });
+  app.setGlobalPrefix('api');
   app.enableVersioning({
     type: VersioningType.URI,
     defaultVersion: '1',
